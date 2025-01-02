d3ccf4e513ec5b9ddfd357533302169a80aebab9
MouadAli02 <alil_mouadali@emsi-edu.ma>
2025-01-02 11:40:39 +0100

    routage statique

--- gateway/src/main/java/org/example/gateway/GatewayApplication.java
+++ gateway/src/main/java/org/example/gateway/GatewayApplication.java
@@ -2,6 +2,9 @@ package org.example.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.ReactiveDiscoveryClient;
+import org.springframework.cloud.gateway.discovery.DiscoveryClientRouteDefinitionLocator;
+import org.springframework.cloud.gateway.discovery.DiscoveryLocatorProperties;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
@@ -13,14 +16,19 @@ public class GatewayApplication {
         SpringApplication.run(GatewayApplication.class, args);
     }
 
-    @Bean
+    //@Bean
     public RouteLocator discoveryClientRouteDefinitionLocator(RouteLocatorBuilder builder) {
         return builder.routes()
-                .route(r -> r.path("/customers/**").uri("lb://customer-service"))
-                .route( r -> r.path("/products/**").uri("http://localhost:8082"))
+                .route(r -> r.path("/customers/**").uri("lb://CUSTOMER-SERVICE"))
+                .route( r -> r.path("/products/**").uri("lb://INVENTORY-SERVICE"))
                 .build();
     }
 
+    @Bean
+    public DiscoveryClientRouteDefinitionLocator dynamicRoutes(ReactiveDiscoveryClient rdc,
+                                                                                       DiscoveryLocatorProperties dlp) {
+        return new DiscoveryClientRouteDefinitionLocator(rdc, dlp);
+    }
 
 }
 
